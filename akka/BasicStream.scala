8d0cbf8973cac51e00009b90932046275128add3
TANG-Jiahua <66163101+TANG-Jiahua@users.noreply.github.com>
2020-06-13 17:49:32 +0200

    Add files via upload

--- akka/BasicStream.scala
+++ akka/BasicStream.scala
@@ -12,26 +12,36 @@ import scala.concurrent.Future
   *
   * Complete the code (replace the ???)
   */
-object BasicStream {
 
+object BasicStream {
   def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("Actor System")
+    implicit val system = ActorSystem("ActorSystem")
     implicit val materializer = ActorMaterializer()
+
+
 
     val numbers = 1 to 1000
 
     val numberSource: Source[Int, NotUsed] = Source.fromIterator(() => numbers.iterator)
 
     //Only let pass even number through the flow
-    val isEvenFlow: Flow[Int, Int, NotUsed] = ???
+    val isEvenFlow: Flow[Int, Int, NotUsed] = Flow[Int].filter(n=>n%2==0)
 
     //Create a Source of even numbers by combining the number Source with the even Flow
-    val evenNumberSource: Source[Int, NotUsed] = ???
+    val evenNumberSource: Source[Int, NotUsed] = numberSource.via(isEvenFlow)
 
     //A Sink that will write its input onto the console
-    val consoleSink: Sink[Int, Future[Done]] = ???
+    val consoleSink: Sink[Int, Future[Done]] = Sink.foreach[Int](x=>println(x))
 
     //Connect the Source with the Sink and run it
-    ???
+    val graph : RunnableGraph[NotUsed] = numberSource.via(isEvenFlow).to(consoleSink)
+    val graph2 : RunnableGraph[NotUsed] = evenNumberSource.to(consoleSink)
+
+    graph.run()
+
+    //    graph2.run()
+
+    system.terminate()
+
   }
 }
